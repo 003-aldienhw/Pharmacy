4c9306451fe5caf9061d32d9626d68acc49459f2
AldienHW <aldienhaidarwahyono@gmail.com>
2026-09-05 22:51:48 +0700

    Adding navbar

---
+++ components/navbar.php
@@ -0,0 +1,5 @@
+<nav class="w-full flex items-center justify-between bg-[#156082]">
+    <a href="">Home</a>
+    <a href="">About</a>
+    <a href="">Yo</a>
+</nav>
--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pharmacy</title>
     <link rel="stylesheet" href="./css/output.css">
 </head>
 <body>
-    <h1 class="text-3xl font-changa">Hello world</h1>
-    <a href="./Views/page1.php">Yo</a>
+    <div class="w-full h-500 flex justify-center bg-[#156082]">
+        <h1 class="font-changa text-3xl text-[#023f5c]">Test</h1>
+    </div>
 </body>
 </html>
