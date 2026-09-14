bce72587cde4469acee9caafc7f7b74bffb63a79
Mwantap <raffa3agas.1212@gmail.com>
2026-09-14 13:48:15 +0700

    adding patitent history on doctor

---
+++ app/Views/pages/doctor/patient_history.php
@@ -0,0 +1,8 @@
+<div class="flex flex-col bg-[#022e42] w-full h-screen justify-center items-center font-changa">
+    <div class="">
+        <div class="flex items-center p-5 bg-[#156082] rounded-lg shadow-xl gap-5">
+            <h2 class="text-3xl text-center text-shadow-sm text-shadow-white">Riwayat Pasien</h2>
+        </div>
+    </div>
+    
+</div>
