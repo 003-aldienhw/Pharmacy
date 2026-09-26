036ea601abe2516bdc45508a7e388c7274166a8a
AldienHW <aldienhaidarwahyono@gmail.com>
2026-09-26 21:57:28 +0700

    Finishing meds request page

--- app/Views/pages/pharmacy/handle_meds_request.php
+++ app/Views/pages/pharmacy/handle_meds_request.php
@@ -1,3 +1,42 @@
+<?php 
+    $meds = [
+        1 => [
+            "dokter" => "Dr. Asep",
+            "pasien" => "Tono",
+            "obat" => "Paracetamol",
+            "jumlah" => "100"
+        ],
+        2 => [
+            "dokter" => "Dr. Ucok",
+            "pasien" => "Toni",
+            "obat" => "Obat tidur",
+            "jumlah" => "100"
+        ],
+        3 => [
+            "dokter" => "Dr. Ucup",
+            "pasien" => "Jason",
+            "obat" => "Racun Sianida",
+            "jumlah" => "100"
+        ],
+    ];
+
+    if (isset($_GET['id'])) {
+        $selected_id = (int)$_GET['id'];
+
+        if (array_key_exists($selected_id, $meds)) {
+            $med = $meds[$selected_id];
+
+            $dokter = htmlspecialchars($med['dokter']);
+            $pasien = htmlspecialchars($med['pasien']);
+            $obat = htmlspecialchars($med['obat']);
+            $jumlah = htmlspecialchars($med['jumlah']);
+        } else {
+            echo "med not found.";
+        }
+    } else {
+        echo "meds was not choosed.";
+    }
+?>
 <?= view('components/pharmacy/navbar') ?>
 <div class="flex flex-col gap-10 p-10 pt-30 justify-center items-center w-full min-h-screen font-changa bg-[#c9c9c9]">
     <div class="flex w-220 justify-between items-center">
@@ -8,6 +47,26 @@
         <div class="w-25"></div>
     </div>
     <div class="flex flex-col w-220 items-center p-10 bg-[#1c80ad] rounded-lg shadow-xl">
-        
+        <div class="flex flex-col w-full gap-5">
+            <div class="flex justify-between items-center">
+                <h1 class="w-full text-center text-lg">Pasien</h1>
+                <h1 class="w-full text-center font-comic font-bold"><?php echo $pasien ?></h1>
+            </div>
+            <div class="flex justify-between items-center">
+                <h1 class="w-full text-center text-lg">Dokter</h1>
+                <h1 class="w-full text-center font-comic font-bold"><?php echo $dokter ?></h1>
+            </div>
+            <div class="flex justify-between items-center">
+                <h1 class="w-full text-center text-lg">Obat</h1>
+                <h1 class="w-full text-center font-comic font-bold"><?php echo $obat ?></h1>
+            </div>
+            <div class="flex justify-between items-center">
+                <h1 class="w-full text-center text-lg">Jumlah</h1>
+                <h1 class="w-full text-center font-comic font-bold"><?php echo $jumlah ?></h1>
+            </div>
+            <div class="flex justify-center">
+                <a href=""><button class="mt-10 p-1 w-30 text-xl text-white bg-[#032196] cursor-pointer rounded-lg">Siapkan</button></a>
+            </div>
+        </div>
     </div>
 </div>
--- app/Views/pages/pharmacy/meds_request.php
+++ app/Views/pages/pharmacy/meds_request.php
@@ -10,24 +10,28 @@
     <div class="flex flex-col w-220 items-center p-10 bg-[#1c80ad] rounded-lg shadow-xl">
         <div class="flex flex-col w-full">
             <div class="flex justify-between mb-5">
+                <h1 class="w-full text-center text-lg">No</h1>
+                <h1 class="w-full text-center text-lg">Pasien</h1>
                 <h1 class="w-full text-center text-lg">Dokter</h1>
                 <h1 class="w-full text-center text-lg">Obat dan Resep</h1>
-                <h1 class="w-full text-center text-lg">Tanggal</h1>
             </div>
             <a href="/apotek/resep?id=1"><div class="flex justify-between items-center font-comic font-bold h-10 hover:bg-white transition-all duration-200 rounded-sm cursor-pointer">
+                <h1 class="w-full text-center text-lg">R01</h1>
+                <h1 class="w-full text-center text-lg">Tono</h1>
                 <h1 class="w-full text-center">Dr. Asep</h1>
                 <h1 class="w-full text-center">Paracetamol 100x sehari</h1>
-                <h1 class="w-full text-center">17 Agustus 1945</h1>
             </div></a>
             <a href="/apotek/resep?id=2"><div class="flex justify-between items-center font-comic font-bold h-10 hover:bg-white transition-all duration-200 rounded-sm cursor-pointer">
+                <h1 class="w-full text-center text-lg">R02</h1>
+                <h1 class="w-full text-center text-lg">Toni</h1>
                 <h1 class="w-full text-center">Dr. Ucok</h1>
                 <h1 class="w-full text-center">Obat Tidur 100x sehari</h1>
-                <h1 class="w-full text-center">17 Agustus 1745</h1>
             </div></a>
             <a href="/apotek/resep?id=3"><div class="flex justify-between items-center font-comic font-bold h-10 hover:bg-white transition-all duration-200 rounded-sm cursor-pointer">
+                <h1 class="w-full text-center text-lg">R03</h1>
+                <h1 class="w-full text-center text-lg">Jason</h1>
                 <h1 class="w-full text-center">Dr. Ucup</h1>
                 <h1 class="w-full text-center">Racun Sianida 100x sehari</h1>
-                <h1 class="w-full text-center">17 Agustus 1899</h1>
             </div></a>
         </div>
     </div>
